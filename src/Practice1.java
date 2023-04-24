c3f0329b1970483a582e90fe2d7a55496ae16de6
CheFF77 <belikto.sandanov@gmail.com>
2023-04-24 21:10:12 +0300

    commit

--- src/Practice1.java
+++ src/Practice1.java
@@ -1,5 +1,13 @@
 public class Practice1 {
-    public static void main(String[] args) {
+    public static void checkUsername(String[] args) {
+        String user1 = "иван иванов";
+        String user2 = "Иван Иванов";
+    }
 
+    private static void checkUserName(String user1, String user2) {
+        if(user1.equalsIgnoreCase(user2)==true)
+            System.out.println("Выберите другое имя пользователя");
+        else
+            System.out.println("Отличное имя!");
     }
 }
